f0b17215ac4bb2463e6c5287d1a86560ce1afb84
Jose <jomaamga@outlook.es>
2024-03-07 12:47:50 +0100

    lsi syntax extension

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
-function contasocListener() {
+async function contasocListener() {
     document.addEventListener('DOMContentLoaded', () => {
         // Obtener el elemento <a> por sus IDs
-        const winLinkElement = document.getElementById('contasocWindows');
-        const linLinkElement = document.getElementById('contasocLinux');
+        let winLinkElement = document.getElementById('contasocWindows');
+        let linLinkElement = document.getElementById('contasocLinux');
 
         // Realizar una solicitud GET a la API de GitHub para obtener la información de la última release
         fetch('https://api.github.com/repos/GalliardDev/Contasoc/releases/latest')
             .then(response => response.json())
             .then(data => {
                 // Obtener el enlace de la última release
-                const winBrowserDownloadUrl = data.assets[1].browser_download_url;
-                const linBrowserDownloadUrl = data.assets[0].browser_download_url;
+                let winBrowserDownloadUrl = data.assets[1].browser_download_url;
+                let linBrowserDownloadUrl = data.assets[0].browser_download_url;
 
                 winLinkElement.href = winBrowserDownloadUrl;
                 linLinkElement.href = linBrowserDownloadUrl;
+            })
+            .catch(error => {
+                console.log('Error:', error);
+            });
+    });
+}
+
+async function lsiSyntaxListener() {
+    document.addEventListener('DOMContentLoaded', () => {
+        // Obtener el elemento <a> por sus IDs
+        let lsiSyntaxElement = document.getElementById('lsiSyntax');
+
+        // Realizar una solicitud GET a la API de GitHub para obtener la información de la última release
+        fetch('https://api.github.com/repos/GalliardDev/LSI-Syntax-for-VSCode/releases/latest')
+            .then(response => response.json())
+            .then(data => {
+                // Obtener el enlace de la última release
+                let downloadUrl = data.assets[0].browser_download_url;
+
+                lsiSyntaxElement.href = downloadUrl;
             })
             .catch(error => {
                 console.log('Error:', error);
@@ -40,8 +60,8 @@ function setHeaderColor() {
 }
 
 async function setPfp() {
-    const response = await fetch('https://api.github.com/users/GalliardDev');
-    const data = await response.json();
-    const pfp = document.getElementById('pfp');
+    let response = await fetch('https://api.github.com/users/GalliardDev');
+    let data = await response.json();
+    let pfp = document.getElementById('pfp');
     pfp.src = data.avatar_url;
 }
